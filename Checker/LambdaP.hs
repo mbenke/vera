8c7a31293168c9296027190e4660b95b25382ba7
Marcin Benke <ben@mimuw.edu.pl>
2013-10-18 11:00:47 +0200

    LambdaP: fix Mvar def

--- Checker/LambdaP.hs
+++ Checker/LambdaP.hs
@@ -8,6 +8,6 @@ data Type = Tvar Name
           | Tall Name Type Type 
           | Tapp Type Term
             
-data Term = Mvar 
+data Term = Mvar Name
           | Mapp Term Term 
           | Mabs Name Type Term
